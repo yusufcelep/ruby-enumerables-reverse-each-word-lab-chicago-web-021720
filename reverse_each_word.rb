2093e14398c0e6ad02ea8e93af86dc4232308a9c
Yusuf Celep <yusufcelep8@gmail.com>
2020-02-09 18:25:42 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
-  array = string.split(" ") #turn string into an array
-  test_array = []
-  array.collect do|string| #iterate over the array
-    test_array << string.reverse #reverse each word in the array
+  array = string.split(" ")
+  new_array = []
+  array.collect do |string|
+    new_array << string.reverse
   end
-  test_array.join(" ")
+  new_array.join(" ")
 end
